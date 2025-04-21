71b9022198e5ba50e6517df0ebe8a03f4641fee6
AniketsinghEC135 <aniket@goodspace.ai>
2025-04-21 17:40:22 +0530

    operations  done

---
+++ opeartions.js
@@ -0,0 +1,11 @@
+let value = 33
+let negvalue = -(value);
+console.log(negvalue);
+console.log(typeof negvalue);
+console.log(typeof value)
+console.log(1+2+"1")
+console.log("1"+"2"+2)
+console.log(1+3*6+8/78)
+console.log(+true)
+console.log(+"")
+
