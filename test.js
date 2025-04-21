6686565b37141a2bdf85a73feae2178f0977c3df
AniketsinghEC135 <aniket@goodspace.ai>
2025-04-21 16:52:50 +0530

    initial upload

---
+++ test.js
@@ -0,0 +1,20 @@
+console.log("aniket")
+let accountemail = "aniket"
+console.log(accountemail)
+const tutorial = 123 
+console.log(tutorial)
+console.log(tutorial)
+console.table([accountemail,tutorial]);
+let lenmbdamarch = "aniketsingh"
+console.table([accountemail,tutorial,lenmbdamarch]);
+console.log(3+32)
+console.log(45+98)
+// code readibiltiy should always be high 
+console.log(7868+87879)
+console.log(78789090)
+console.log(89*8)
+let loginih = 18 
+let state 
+console.log(state)
+console.log(typeof "hiyesj")
+console.log(typeof null)
