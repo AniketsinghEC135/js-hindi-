846fc7e5a10ef33ca2a4ff8e1eeca7f7ce7bdb61
AniketsinghEC135 <aniket@goodspace.ai>
2025-04-22 15:52:12 +0530

    memory(stack and hep )

---
+++ memory.js
@@ -0,0 +1,14 @@
+let ashish = "dodwal"
+let aniket = ashish
+console.log(aniket)
+
+// the primary datatypes in the javascript are stored in the stack memory 
+let function1 = {
+    email:"aniket@goodspace.ai"
+}
+let function2 = function1
+function2.email="saksham@goodspace.ai"
+
+console.log(function1)
+console.log(function2)
+// the seconday datatypes in the javasript are stored in the heap memory 
